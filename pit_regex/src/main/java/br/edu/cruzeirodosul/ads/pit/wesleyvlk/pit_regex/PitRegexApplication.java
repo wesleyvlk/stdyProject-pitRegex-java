20a51f537dd40a510dce686c3893a38149be15e1
wesleyvlk <wesleymuniz20@gmail.com>
2023-05-06 12:41:57 -0300

    Feat! pitRegex project structure
    Signed-off-by: wesleyvlk <wesleymuniz20@gmail.com>

---
+++ pit_regex/src/main/java/br/edu/cruzeirodosul/ads/pit/wesleyvlk/pit_regex/PitRegexApplication.java
@@ -0,0 +1,13 @@
+package br.edu.cruzeirodosul.ads.pit.wesleyvlk.pit_regex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PitRegexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PitRegexApplication.class, args);
+    }
+
+}
